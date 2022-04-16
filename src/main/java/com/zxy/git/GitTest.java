5258029c890b275aa8e986eec9ed88bb8f587d77
yun-cell <74649439+yun-cell@users.noreply.github.com>
2022-04-16 23:31:08 +0800

    pull commit

--- src/main/java/com/zxy/git/GitTest.java
+++ src/main/java/com/zxy/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("master!!!");
         System.out.println("push");
+        System.out.println("pull");
     }
 
 }
